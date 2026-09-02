9b6e1a042012097006a78eb16459a1648633124d
NullPointerCzar <sarwagyaformal@gmail.com>
2026-09-02 14:13:45 +0545

    fix: minor spelling error

--- src/phase0/exceptions.py
+++ src/phase0/exceptions.py
@@ -5,7 +5,7 @@ class ApplicationError(Exception):
     """Base class for all application errors."""
     pass
 
-class FetchDataError(ApplicatonError):
+class FetchDataError(ApplicationError):
     """Raised when there is an error fetching data."""
     def __init__(self, url:str, status_code:int)-> None:
         self.url = url
